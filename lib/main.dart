1f6b4e0a38b0e52480d53e57e55fbc7358e0de95
Roshan Pokal <roshukv02@gmail.com>
2022-10-31 01:02:51 +0530

    first commit - initialize

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text('Hello')
+        ),
+      )
+    );
+  }
+}
